a2ad4e66f1020b3f8b7d1fcd0b3a36c1b1586fd6
Craig Gomez <craigacgomez@gmail.com>
2015-10-28 19:59:35 -0700

    Do not force OpenJDK on Linux
    Allow building on Linux with either OpenJDK
    or the Sun/Oracle JDK.

--- core/main.mk
+++ core/main.mk
@@ -175,10 +175,6 @@ endif
 #
 # For Java 1.7, we require OpenJDK on linux and Oracle JDK on Mac OS.
 requires_openjdk := false
-ifeq ($(HOST_OS), linux)
-requires_openjdk := true
-endif
-
 
 # Check for the current jdk
 ifeq ($(requires_openjdk), true)
